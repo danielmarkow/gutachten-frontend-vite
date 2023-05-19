1d52b8cd80c1952ac1bfda35007692e46d6fb091
Daniel Markow <d_markow@icloud.com>
2023-05-19 07:35:03 +0200

    setting inital editor state from remote source

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,10 +1,8 @@
-import axios from "axios";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
-import { RemoteStoragePlugin } from "../plugins/RemoteStoragePlugin";
-import { useQuery } from "react-query";
+import { RestoreFromStoragePlugin } from "../plugins/RestoreFromStoragePlugin";
 
 type LexicalEditorProps = {
   config: Parameters<typeof LexicalComposer>["0"]["initialConfig"];
@@ -18,7 +16,7 @@ export function LexicalEditor(props: LexicalEditorProps) {
         placeholder={<Placeholder />}
         ErrorBoundary={LexicalErrorBoundary}
       />
-      <RemoteStoragePlugin namespace={props.config.namespace} />
+      <RestoreFromStoragePlugin namespace={props.config.namespace} />
     </LexicalComposer>
   );
 }
@@ -32,53 +30,34 @@ const Placeholder = () => {
 };
 
 export function Editor({ ga_id }: { ga_id: string }) {
-  const editorContentQuery = useQuery({
-    queryFn: async () => {
-      const res = await axios.get(
-        "http://localhost:8000/api/gutachten/" + ga_id
-      );
-      return res.data;
-    },
-    enabled: ga_id !== undefined,
-    onError: () => {
-      console.error("error retrieving gutachten data");
-    },
-  });
-
   return (
     <>
-      {editorContentQuery.isSuccess && editorContentQuery.data && (
-        <>
-          {JSON.stringify(editorContentQuery.data.ga)}
-          <div
-            id="editor-wrapper"
-            className={
-              "relative prose prose-slate prose-p:my-0 prose-headings:mb-4 prose-headings:mt-2"
-            }
-          >
-            <LexicalEditor
-              config={{
-                namespace: ga_id,
-                // editorState: editorContentQuery.data.ga,
-                theme: {
-                  root: "p-4 border-slate-500 border-2 rounded h-full min-h-[200px] focus:outline-none focus-visible:border-black",
-                  link: "cursor-pointer",
-                  text: {
-                    bold: "font-semibold",
-                    underline: "underline",
-                    italic: "italic",
-                    strikethrough: "line-through",
-                    underlineStrikethrough: "underlined-line-through",
-                  },
-                },
-                onError: (error) => {
-                  console.log(error);
-                },
-              }}
-            />
-          </div>
-        </>
-      )}
+      <div
+        id="editor-wrapper"
+        className={
+          "relative prose prose-slate prose-p:my-0 prose-headings:mb-4 prose-headings:mt-2"
+        }
+      >
+        <LexicalEditor
+          config={{
+            namespace: ga_id,
+            theme: {
+              root: "p-4 border-slate-500 border-2 rounded h-full min-h-[200px] focus:outline-none focus-visible:border-black",
+              link: "cursor-pointer",
+              text: {
+                bold: "font-semibold",
+                underline: "underline",
+                italic: "italic",
+                strikethrough: "line-through",
+                underlineStrikethrough: "underlined-line-through",
+              },
+            },
+            onError: (error) => {
+              console.log(error);
+            },
+          }}
+        />
+      </div>
     </>
   );
 }
---
+++ src/plugins/RestoreFromStoragePlugin.tsx
@@ -0,0 +1,55 @@
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import type { EditorState } from "lexical";
+
+import axios from "axios";
+import { useMutation, useQuery } from "react-query";
+import { useEffect, useState } from "react";
+
+import { debounce } from "../utils/debounce";
+
+export function RestoreFromStoragePlugin({ namespace }: { namespace: string }) {
+  const [editor] = useLexicalComposerContext();
+  const [isFirstRender, setIsFirstRender] = useState(true);
+
+  const saveContentMut = useMutation({
+    mutationFn: async ({ ga }: { ga: EditorState }) => {
+      await axios.put("http://localhost:8000/api/gutachten/" + namespace, {
+        ga,
+      });
+    },
+    onError: () => {
+      console.error("something went wrong updating the remote storage");
+    },
+  });
+
+  const editorContentQuery = useQuery({
+    queryFn: async () => {
+      const res = await axios.get(
+        "http://localhost:8000/api/gutachten/" + namespace
+      );
+      return res.data;
+    },
+    enabled: namespace !== undefined,
+    onError: () => {
+      console.error("error retrieving gutachten data");
+    },
+    onSuccess: (data) => {
+      if (isFirstRender) {
+        setIsFirstRender(false);
+        if (Object.keys(data).length > 0) {
+          const initialEditorState = editor.parseEditorState(data.ga);
+          editor.setEditorState(initialEditorState);
+        }
+      }
+    },
+  });
+
+  const debouncedSaveContent = debounce(saveContentMut.mutate, 1000);
+
+  const onChange = (editorState: EditorState) => {
+    debouncedSaveContent({ ga: editorState });
+  };
+
+  return <OnChangePlugin onChange={onChange} />;
+}
