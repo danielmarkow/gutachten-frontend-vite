8104b7e746d520e5347f9673946447d3a0727d09
Daniel Markow <d_markow@icloud.com>
2023-05-26 07:46:13 +0200

    added color preview in color selector

--- src/components/EditSnippetModal.tsx
+++ src/components/EditSnippetModal.tsx
@@ -27,16 +27,16 @@ const schema = z.object({
   }),
 });
 
-type FormValues = {
-  oberpunkt: string;
-  unterpunkt: string;
-  farbe: typeof COLOR_LIST;
-};
-
 type Color = {
   name: string;
   value: string;
   twind: string;
+};
+
+type FormValues = {
+  oberpunkt: string;
+  unterpunkt: string;
+  farbe: Color;
 };
 
 const COLOR_LIST = [
@@ -169,7 +169,12 @@ export default function EditSnippetModal({
                                 <div className="relative mt-2">
                                   <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                     <span className="block truncate">
-                                      {selectedColor.name}
+                                      <div className="flex align-middle gap-1">
+                                        <Square2StackIcon
+                                          className={`h-5 w-5 ${selectedColor?.twind}`}
+                                        />{" "}
+                                        {selectedColor?.name}
+                                      </div>
                                     </span>
                                     <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                                       <ChevronUpDownIcon
@@ -201,30 +206,21 @@ export default function EditSnippetModal({
                                         >
                                           {({ selected, active }) => (
                                             <>
-                                              <span
-                                                className={classNames(
-                                                  selected
-                                                    ? "font-semibold"
-                                                    : "font-normal",
-                                                  "block truncate"
-                                                )}
-                                              >
-                                                <div className="flex align-middle gap-1">
-                                                  <Square2StackIcon
-                                                    className={`h-5 w-5 ${color.twind}`}
-                                                  />
-                                                  <p
-                                                    className={classNames(
-                                                      selected
-                                                        ? "font-semibold"
-                                                        : "font-normal",
-                                                      "block truncate"
-                                                    )}
-                                                  >
-                                                    {color.name}
-                                                  </p>
-                                                </div>
-                                              </span>
+                                              <div className="flex align-middle gap-1">
+                                                <Square2StackIcon
+                                                  className={`h-5 w-5 ${color.twind}`}
+                                                />
+                                                <p
+                                                  className={classNames(
+                                                    selected
+                                                      ? "font-semibold"
+                                                      : "font-normal",
+                                                    "block truncate"
+                                                  )}
+                                                >
+                                                  {color.name}
+                                                </p>
+                                              </div>
 
                                               {selected ? (
                                                 <span
