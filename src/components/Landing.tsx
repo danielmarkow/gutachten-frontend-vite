480a01dad8c5e93e79d20d9fb05786eded3f9e9d
Daniel Markow <d_markow@icloud.com>
2023-05-31 09:13:43 +0200

    Meine Gutachten button on landing

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,4 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "wouter";
 
 export default function Landing() {
   const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
@@ -7,13 +8,18 @@ export default function Landing() {
     <>
       <p>landing</p>
       {isAuthenticated ? (
-        <button
-          onClick={() =>
-            logout({ logoutParams: { returnTo: window.location.origin } })
-          }
-        >
-          Log Out
-        </button>
+        <>
+          <button
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Log Out
+          </button>
+          <Link href="/gutachten">
+            <a>Meine Gutachten</a>
+          </Link>
+        </>
       ) : (
         <button onClick={() => loginWithRedirect()}>Log In</button>
       )}
